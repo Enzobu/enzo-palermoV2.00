25e2179244298e4cae82b61a199796c4b11651cd
Enzobu <enzo.palermo@epsi.fr>
2023-11-02 16:08:56 +0100

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include('part/head.php'); ?>
+    <link rel="stylesheet" href="style/presentation/presentation.css">
+    <title>Document</title>
+</head>
+<body class="body">
+    <header class="header">
+        <?php include('part/header.php'); ?>
+    </header>
+
+    <main class="main">
+        <section class="presentation">
+            <?php include('part/presentation.php'); ?>
+        </section>
+        <section class="projets">
+            <?php include('part/projets.php'); ?>
+        </section>
+        <section class="a-propos">
+            <?php include('part/a-propos.php'); ?>
+        </section>
+        <section class="cv">
+            <?php include('part/cv.php'); ?>
+        </section>
+    </main>
+
+    <footer class="footer">
+        <?php include('part/footer.php'); ?>
+    </footer>
+</body>
+</html>

5f78eaf85ddfa204e10c256a22e9337209cec4ed
Enzobu <enzo.palermo@epsi.fr>
2023-11-02 16:09:15 +0100

    Create php part of files

---
+++ php/part/header.php
@@ -0,0 +1,23 @@
+<nav class="nav">
+    <ul class="nav-list">
+        <li class="left-link">
+            <ul class="sub-nav-list">
+                <li class="nav-item"><a href="/" class="link-nav-item"><img src="/images/logo_sans_fond.png" alt="Logo Enzo Palermo"></a></li>
+            </ul>
+        </li>
+        <li class="center-link">
+            <ul class="sub-nav-list">
+                <li class="nav-item"><a href="#top" class="link-nav-item">Accueil</a></li>
+                <li class="nav-item"><a href="#projets" class="link-nav-item">Projets</a></li>
+                <li class="nav-item"><a href="#a-propos" class="link-nav-item">À propos</a></li>
+                <li class="nav-item"><a href="#cv" class="link-nav-item">CV</a></li>
+            </ul>
+        </li>
+        <li class="right-link">
+            <ul class="sub-nav-list">
+                <li class="nav-item"><a href="/contact" class="link-nav-item">Contact</a></li>
+                <li class="nav-item"><a href="/login" class="link-nav-item">Login</a></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
---
+++ php/part/presentation.php
@@ -0,0 +1,28 @@
+<div class="left-presentation">
+    <div class="img-presentation">
+        <div class="cadre-image"></div>
+        <img src="/images/PALERMO_enzo.jpg" alt="">
+    </div>
+</div>
+<div class="right-presentation">
+    <div class="boutons">
+        <div class="bouton bouton-rouge"></div>
+        <div class="bouton bouton-jaune"></div>
+        <div class="bouton bouton-vert"></div>
+    </div>
+    <div class="nums-ligne">
+        <div class="num-ligne">1</div>
+        <div class="num-ligne">2</div>
+        <div class="num-ligne">3</div>
+        <div class="num-ligne">4</div>
+        <div class="num-ligne">5</div>
+    </div>
+    <div class="texte-container" id="texte-container">
+        <p class="texte" id="texte"></p>
+    </div>
+    <script src="/js/defilementText.js"></script>
+    <script src="/js/confettiss.js"></script>
+</div>
+<div class="bottom-presentation">
+
+</div>  

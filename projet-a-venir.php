93288a4e6ff2f8c29c8e82476dcad89caa30d5f9
Enzobu <enzo.palermo@epsi.fr>
2024-01-07 11:51:35 +0100

    create protjet-a-venir (temp)

---
+++ projet-a-venir.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include('php/part/head.php'); ?>
+    <link rel="stylesheet" href="style/projet-a-venir/projet-a-venir.css">
+    <title>Document</title>
+</head>
+<body class="body">
+    <header class="header">
+        <?php include('php/part/header.php'); ?>
+    </header>
+
+    <main class="main">
+        <div class="title">
+            Projet bientôt disponible !
+        </div>
+    </main>
+
+</body>
+</html>

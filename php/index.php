23bd864656512c29a68b37c8dda268ba22b71e5e
Enzobu <enzo.palermo@epsi.fr>
2024-01-07 11:48:35 +0100

    add id from section

--- php/index.php
+++ php/index.php
@@ -3,6 +3,9 @@
 <head>
     <?php include('part/head.php'); ?>
     <link rel="stylesheet" href="style/presentation/presentation.css">
+    <link rel="stylesheet" href="style/projets/projets.css">
+    <link rel="stylesheet" href="style/a-propos/a-propos.css">
+    <link rel="stylesheet" href="style/cv/cv.css">
     <title>Document</title>
 </head>
 <body class="body">
@@ -11,16 +14,16 @@
     </header>
 
     <main class="main">
-        <section class="presentation">
+        <section class="presentation" id="presentation">
             <?php include('part/presentation.php'); ?>
         </section>
-        <section class="projets">
+        <section class="projets" id="projets">
             <?php include('part/projets.php'); ?>
         </section>
-        <section class="a-propos">
+        <section class="a-propos" id="a-propos">
             <?php include('part/a-propos.php'); ?>
         </section>
-        <section class="cv">
+        <section class="cv" id="cv">
             <?php include('part/cv.php'); ?>
         </section>
     </main>

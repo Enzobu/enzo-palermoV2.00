4d5157f8a9e9322312c0d7caa0587a6e618a60fe
Enzobu <enzo.palermo@epsi.fr>
2024-01-07 11:54:13 +0100

    create project file

---
+++ php/part/projets.php
@@ -0,0 +1,152 @@
+<h2 class="title">Projets</h2>
+<h3 class="sub-title">
+    Durant mes études, j'ai eu bon nombre 
+    de projets à réaliser. Certains d'entre 
+    eux sont plus personnel.
+</h3>
+<div class="carousel-container">
+    <div class="carousel">
+        <div class="card" id="card1">
+            <div class="card-title">Workshop SN1</div>
+            <div class="card-image">
+                <img src="/images/projets/" alt="">
+            </div>
+            <p class="card-description">
+                Workshop : Projet (victorieux !) de début d'année : Création d'une 
+                innovation utilisable dans la vie de tout les jours : 
+                SafeBike : Un système de clignottant et bien plus pour vélo.
+            </p>
+            <div class="card-buttons">
+                <a href="">
+                <div class="card-button">
+                        <img src="/images/projets/github.svg" alt="">
+                    </div>
+                </a>
+                <a href="/projet-a-venir">
+                <div class="card-button">
+                        <img src="/images/projets/voir.svg" alt="">
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="card" id="card2">
+            <div class="card-title">Projet Neptune</div>
+            <div class="card-image">
+                <img src="/images/projets/projet-neptune.png" alt="">
+            </div>
+            <p class="card-description">
+                L'hotel Neptume est un projet visant à réaliser le site 
+                de l'hotel Neptune. Il à été réaliser durant le premier 
+                projet de l'année.
+            </p>
+            <div class="card-buttons">
+                <a href="">
+                <div class="card-button">
+                        <img src="/images/projets/github.svg" alt="">
+                    </div>
+                </a>
+                <a href="/projet-a-venir">
+                <div class="card-button">
+                        <img src="/images/projets/voir.svg" alt="">
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="card" id="card3">
+            <div class="card-title">Hi-Mix</div>
+            <div class="card-image">
+                <img src="/images/projets/hi-mix.png" alt="">
+            </div>
+            <p class="card-description">
+                Hi-mix est un projet visant à réaliser le site de la marque : 
+                un site de vente d'alimentation sportive. Il à été développer 
+                durant un cours et un porjet.
+            </p>
+            <div class="card-buttons">
+                <a href="">
+                <div class="card-button">
+                        <img src="/images/projets/github.svg" alt="">
+                    </div>
+                </a>
+                <a href="/projet-a-venir">
+                <div class="card-button">
+                        <img src="/images/projets/voir.svg" alt="">
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="card active" id="card4">
+            <div class="card-title">Mon site web</div>
+            <div class="card-image">
+                <img src="/images/projets/enzo-palermo.com.png" alt="">
+            </div>
+            <p class="card-description">
+                Je crée mon site pour mettre en avant mon portfolio, mon CV ainsi 
+                que les projets réalisés.
+            </p>
+            <div class="card-buttons">
+                <a href="">
+                <div class="card-button">
+                        <img src="/images/projets/github.svg" alt="">
+                    </div>
+                </a>
+                <a href="/projet-a-venir">
+                <div class="card-button">
+                        <img src="/images/projets/voir.svg" alt="">
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="card" id="card5">
+            <div class="card-title">Climat-2030.org</div>
+            <div class="card-image">
+                <img src="/images/projets/climat-2030.png" alt="">
+            </div>
+            <p class="card-description">
+                Stage en entreprise : Création du site de l'entreprise Climat-2030
+            </p>
+            <div class="card-buttons">
+                <a href="">
+                <div class="card-button">
+                        <img src="/images/projets/github.svg" alt="">
+                    </div>
+                </a>
+                <a href="/projet-a-venir">
+                <div class="card-button">
+                        <img src="/images/projets/voir.svg" alt="">
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="card" id="card6">
+            <div class="card-title">Workshop SN2</div>
+            <div class="card-image">
+                <img src="/images/projets/learn-it.png" alt="">
+            </div>
+            <p class="card-description">
+                Workshop : Projet de début d'année : Création de Learn-it : Un site web proposant 
+                des formation en ligne.
+            </p>
+            <div class="card-buttons">
+                <a href="">
+                <div class="card-button">
+                        <img src="/images/projets/github.svg" alt="">
+                    </div>
+                </a>
+                <a href="/projet-a-venir">
+                <div class="card-button">
+                        <img src="/images/projets/voir.svg" alt="">
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="carousel-selector">
+        <div class="selector" id="selector1"></div>
+        <div class="selector" id="selector2"></div>
+        <div class="selector active" id="selector3"></div>
+        <div class="selector" id="selector4"></div>
+        <div class="selector" id="selector5"></div>
+        <div class="selector" id="selector6"></div>
+    </div>
+</div>

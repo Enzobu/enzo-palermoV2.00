bb3e5673729c753b20fb250f9e3ccb4d908e8fcb
Enzobu <enzo.palermo@epsi.fr>
2024-02-03 17:04:18 +0100

    cv section

---
+++ php/part/cv.php
@@ -0,0 +1,11 @@
+<h2 class="title">Mon CV</h2>
+<div class="cv-container">
+    <p class="description">
+        Vous voulez en savoir plus sur moi, 
+        mes expériences professionnelles ? 
+        Jetez un oeil à mon CV !
+    </p>
+    <a href="/cv/CV_Enzo_Palermo.pdf" download>
+        <div class="download-button">Télécharger mon CV</div>
+    </a>
+</div>

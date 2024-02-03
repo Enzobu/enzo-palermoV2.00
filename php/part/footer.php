c5d8005434bdc4c393ba59bfc21584b6da28373c
Enzobu <enzo.palermo@epsi.fr>
2024-02-03 17:04:24 +0100

    footer section

---
+++ php/part/footer.php
@@ -0,0 +1,53 @@
+<div class="footer-logo">
+    <img src="/images/header/logo_sans_fond.png" alt="">
+</div>
+<div class="footer-contact">
+    <div class="title">Me contacter</div>
+    <div class="contacts">
+        <a href="mailto:enzo.palermo@ecoles-epsi.net" class="footer-link">
+            <div class="contact">
+                <img src="/images/footer/email.svg" alt="" class="contact-img">
+                <div class="contact-content">
+                    enzo.palermo@ecoles-epsi.net
+                </div>
+            </div>
+        </a>
+        <div class="contact">
+            <img src="/images/footer/telephoner.svg" alt="" class="contact-img">
+            <div class="contact-content">07 68 63 87 62</div>
+        </div>
+        <div class="contact">
+            <img src="/images/footer/accueil.svg" alt="" class="contact-img">
+            <div class="contact-content">St Clément de Rivière</div>
+        </div>
+    </div>
+</div>
+<div class="footer-social-media">
+    <div class="title">Me retrouver</div>
+    <div class="social-medias">
+        <a href="" class="footer-link">
+            <div class="social-media">
+                <img src="/images/footer/linkedin.svg" alt="" class="social-media-img">
+                <div class="social-media-content">Linkedin</div>
+            </div>
+        </a>
+        <a href="" class="footer-link">
+            <div class="social-media">
+                <img src="/images/footer/instagram.svg" alt="" class="social-media-img">
+                <div class="social-media-content">Instagram</div>
+            </div>
+        </a>
+        <a href="" class="footer-link">
+            <div class="social-media">
+                <img src="/images/footer/globe.svg" alt="" class="social-media-img">
+                <div class="social-media-content">enzo-palermo.com</div>
+            </div>
+        </a>
+        <a href="" class="footer-link">
+            <div class="social-media">
+                <img src="/images/footer/github.svg" alt="" class="social-media-img">
+                <div class="social-media-content">GitHub</div>
+            </div>
+        </a>
+    </div>
+</div>

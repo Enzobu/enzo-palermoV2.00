f6e688ada2ad304d44ea98f3ceb7136584389d09
Enzobu <enzo.palermo@epsi.fr>
2024-02-03 17:04:12 +0100

    a-propos section

---
+++ php/part/a-propos.php
@@ -0,0 +1,89 @@
+<h2 class="title">À propos</h2>
+<div class="a-propos-container">
+    <div class="a-propos-description">
+        <div class="title">À propos de moi</div>
+        <p class="content">
+            Je suis étudiant en deuxième année à 
+            l'EPSI Montpellier en spécialité développement. 
+            J'aime beaucoup le dévoloppement web. Je touche 
+            à plusieurs langages de programmation.
+        </p>
+    </div>
+    <div class="cloud-container">
+        <div class="title">Mes compétences</div>
+        <div class="cloud" id="cloud">
+            <div class="skill">
+                <div class="description-skill">
+                    <span class="name-skill">HTML</span> est un langage de balisage pour structurer le contenu des pages Web.
+                </div>
+                <img src="/images/a-propos/HTML.svg" alt="HTML" class="skill-icon">
+            </div>
+            <div class="skill">
+                <div class="description-skill">
+                    <span class="name-skill">CSS</span> est un langage de style pour la présentation visuelle des pages Web.
+                </div>
+                <img src="/images/a-propos/CSS.svg" alt="CSS" class="skill-icon">
+            </div>
+            <div class="skill">
+                <div class="description-skill">
+                    <span class="name-skill">SASS</span> / SCSS est une extension de CSS avec des fonctionnalités étendues pour la gestion des styles.
+                </div>
+                <img src="/images/a-propos/SASS.svg" alt="SASS / SCSS" class="skill-icon">
+            </div>
+            <div class="skill">
+                <div class="description-skill">
+                    <span class="name-skill">JavaScript</span> est un langage de programmation pour le développement côté client des pages Web.
+                </div>
+                <img src="/images/a-propos/JavaScript.svg" alt="JavaScript" class="skill-icon">
+            </div>
+            <div class="skill">
+                <div class="description-skill">
+                    <span class="name-skill">PHP</span> est un langage de script côté serveur pour le développement Web dynamique.
+                </div>
+                <img src="/images/a-propos/PHP.png" alt="PHP" class="skill-icon">
+            </div>
+            <div class="skill">
+                <div class="description-skill">
+                    <span class="name-skill">MySQL</span> est un système de gestion de base de données relationnelles SQL.
+                </div>
+                <img src="/images/a-propos/MySQL.svg" alt="MySQL" class="skill-icon">
+            </div>
+            <div class="skill">
+                <div class="description-skill">
+                    <span class="name-skill">MongoDB</span> est une base de données NoSQL orientée document, stockant les données au format BSON.
+                </div>
+                <img src="/images/a-propos/MongoDB.svg" alt="MongoDB" class="skill-icon">
+            </div>
+            <div class="skill">
+                <div class="description-skill">
+                    <span class="name-skill">Linux</span> est un système d'exploitation open-source largement utilisé dans les serveurs et systèmes embarqués.
+                </div>
+                <img src="/images/a-propos/Linux.png" alt="Linux" class="skill-icon">
+            </div>
+            <div class="skill">
+                <div class="description-skill">
+                    <span class="name-skill">Python</span> est un langage de programmation polyvalent, utilisé pour le développement Web, l'automatisation et plus encore.
+                </div>
+                <img src="/images/a-propos/Python.png" alt="Python" class="skill-icon">
+            </div>
+            <div class="skill">
+                <div class="description-skill">
+                    <span class="name-skill">Flutter</span> est un framework open-source de développement d'applications mobiles multiplateformes avec Dart.
+                </div>
+                <img src="/images/a-propos/Flutter.svg" alt="Flutter" class="skill-icon">
+            </div>
+        </div>
+    </div>
+    <!-- <script src="/js/mouvementLogo.js"></script> -->
+</div>
+
+<!-- Langage de balisage pour créer des pages web structurées et sémantiques.
+Langage de style permettant de mettre en forme et de styliser les pages web.
+Extension de CSS avec des fonctionnalités étendues et une syntaxe plus concise.
+Langage de programmation côté client pour rendre les pages web interactives et dynamiques.
+Langage de script côté serveur pour développer des applications web dynamiques.
+Système de gestion de base de données relationnelle (SGBDR) utilisé avec PHP pour stocker des données.
+Base de données NoSQL orientée documents, utilisée pour stocker et gérer des données de manière flexible.
+Système d'exploitation open source prisé pour sa stabilité et sa flexibilité dans les environnements serveur.
+Langage de programmation polyvalent, utilisé pour le développement web, la science des données, l'automatisation, etc.
+Cadre de développement open source de Google pour créer des applications multiplateformes avec un seul code source. -->

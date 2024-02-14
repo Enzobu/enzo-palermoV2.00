d2b4c45cd415c10eec1a379dcd990fb1b7cabe47
Enzobu <enzo.palermo@epsi.fr>
2024-02-14 16:20:30 +0100

    Create 404.php

--- php/404.php
+++ php/404.php
@@ -1,1 +1,27 @@
-404
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include('part/head.php'); ?>
+    <link rel="stylesheet" href="/style/404/404.css">
+    <title>Document</title>
+</head>
+<body class="body">
+    <header class="header">
+        <?php include('part/header.php'); ?>
+    </header>
+
+    <main class="main">
+        <div class="err-404-container">
+            <h1 class="title">404 ;(</h1>
+            <h2 class="sub-title">La page demandée n'existe pas ...</h2>
+            <div class="link-404-button">
+                <a href="/">Retour à l'acceuil</a>
+            </div>
+        </div>
+    </main>
+
+    <footer class="footer">
+        <?php include('part/footer.php'); ?>
+    </footer>
+</body>
+</html>

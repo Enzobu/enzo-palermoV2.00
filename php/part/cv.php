e17480f605c14494260ff3325b8201f8eda93da9
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-04-29 10:58:07 +0200

    Update cv.php

--- php/part/cv.php
+++ php/part/cv.php
@@ -5,7 +5,7 @@
         mes expériences professionnelles ? 
         Jetez un oeil à mon CV !
     </p>
-    <a href="https://www.canva.com/design/DAFxgne3gcY/Yc4db19B7TeRosovaZhyrA/view?utm_content=DAFxgne3gcY&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h412048189f">
+    <a href="https://www.canva.com/design/DAFxgne3gcY/Yc4db19B7TeRosovaZhyrA/view?utm_content=DAFxgne3gcY&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h412048189f" target="_blank">
         <div class="download-button">Télécharger mon CV</div>
     </a>
 </div>

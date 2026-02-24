a525aaf7a5002637074405d644209d86c431feda
Enzo Palermo <118272358+Enzobu@users.noreply.github.com>
2026-02-24 21:31:13 +0100

    Update student year and job title in defilementText.js

--- js/defilementText.js
+++ js/defilementText.js
@@ -1,4 +1,4 @@
-const text = "Je suis étudiant en seconde année à l'EPSI Montpellier en spécialité développeur. J'aime beaucoup le développement web. Si vous voulez en savoir d'avantage, regardez en dessous ! :)";
+const text = "Je suis étudiant en quatrième année à l'EPSI Montpellier en spécialité développeur. Je suis développeur backend chez CA-TS. Si vous voulez en savoir d'avantage, regardez en dessous ! :)";
 const textContainer = document.getElementById("texte");
 let index = 0;
 
